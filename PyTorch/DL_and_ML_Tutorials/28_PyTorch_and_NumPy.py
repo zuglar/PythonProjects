203c1bf0d922da33d0fcc613777bcac962011de8
zuglar <uglarz@gmail.com>
2024-10-06 18:15:10 +0200

    PyTorch and OPenCV

--- PyTorch/DL_and_ML_Tutorials/28_PyTorch_and_NumPy.py
+++ PyTorch/DL_and_ML_Tutorials/28_PyTorch_and_NumPy.py
@@ -7,3 +7,12 @@ from numpy.matrixlib.defmatrix import matrix
 from sympy import print_tree
 
 print(f'PyTorch version: {torch.__version__}\n')
+
+print('PyTorch tensors and NumPy\n'
+      'NumPy is a popular scientific Python numerical computing library.\n'
+      'And because of this, PyTorc has functionality to interact with it.\n'
+      '* Data in NumPy, want in PyTorch tensor ->  torch.from_numpy(ndarray)\n'
+      '* PyTorch tensor->Numpy-> torch.Tensor.numpy()\n\n')
+
+print('NumPy array to tensor')
+
